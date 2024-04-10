3d362de9382df48ec90043a2aca2bab1a3a682cc
Inés <146125296+inesguillen@users.noreply.github.com>
2024-04-10 21:34:35 +0200

    Update code.sql

--- 1.4/1. attribute endorsed/code.sql
+++ 1.4/1. attribute endorsed/code.sql
@@ -1,21 +1,17 @@
-CREATE OR REPLACE TRIGGER endorsed
-AFTER INSERT ON Posts
+CREATE OR REPLACE TRIGGER endorsed  
+BEFORE INSERT or UPDATE ON Posts
 FOR EACH ROW
-DECLARE
-	purchased NUMBER;
 BEGIN
-	SELECT count(*) INTO purchased FROM Orders_Clients a
-	JOIN Client_Lines b ON a.username = b.username
-	AND a.orderdate = b.orderdate
-	WHERE a.username = :new.username;
-	IF purchased > 0 THEN
-		UPDATE Posts 
-		SET endorsed = 'Y'
-		WHERE postdate = :new.postdate;
-	ELSE
-		UPDATE Posts 
-		SET endorsed = 'N'
-		WHERE postdate = :new.postdate;
-	END IF;
-END;
-
+	DECLARE purchased DATE := NULL;
+	BEGIN
+		SELECT max(a.ORDERDATE) INTO purchased 
+		FROM Orders_Clients a, Client_Lines b
+		WHERE a.username = b.username AND a.orderdate = b.orderdate
+		AND b.barcode = :new.barcode AND a.username = :new.username;
+		IF purchased IS NOT NULL THEN
+		   :new.endorsed := purchased;
+		ELSE
+		   :new.endorsed := NULL;
+		END IF;
+	END;
+END endorsed;
