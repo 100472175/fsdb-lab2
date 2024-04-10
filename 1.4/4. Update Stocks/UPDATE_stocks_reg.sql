1811d037ffef505455024f8c768299d8c517e35c
100472175 <100472175@alumnos.uc3m.es>
2024-04-10 19:36:17 +0200

    Updates trigger 4.4

--- 1.4/4. Update Stocks/UPDATE_stocks_reg.sql
+++ 1.4/4. Update Stocks/UPDATE_stocks_reg.sql
@@ -1,17 +1,5 @@
--- UPDATE stocks:
--- After insertion to anonym_lines or client_lines, check the stop of that product in the table references
--- IF the current stock is greater than the order quantity, UPDATE the stock to stock-order_quantity.
-
--- IF the current stock is less than the minimum stock, UPDATE the stock to 0 AND the order quantity to the stock.
--- Cehck there is no supply line with the same product AND status = 'D'.
--- Create a replacement order with:
-     -- orderdate as sysdate, 
-     -- reference FROM the one that has the minimum cost AND the same product FROM the supply_lines,
-     -- units = minimum stock FROM the references table,
-     -- payment = '1234567890.12'
-
 CREATE OR REPLACE TRIGGER UPDATE_stocks_reg
-AFTER INSERT ON Client_Lines
+BEFORE INSERT ON Client_Lines
 FOR each ROW
 DECLARE
     v_stock references.cur_stock % TYPE;
@@ -21,8 +9,8 @@ DECLARE
     v_supplier providers.taxID % TYPE;
     v_supplier_cost supply_lines.cost % TYPE;
 BEGIN
-    SELECT cur_stock, min_stock
-    INTO v_stock, v_min_stock
+    SELECT cur_stock, min_stock, max_stock
+    INTO v_stock, v_min_stock, v_max_stock
     FROM references
     WHERE barcode = :new.barcode; 
     IF v_stock - :new.quantity > v_min_stock then
@@ -33,27 +21,27 @@ BEGIN
         UPDATE references 
             SET cur_stock = 0 
             WHERE barcode = :new.barCode;
-        UPDATE Client_Lines 
-            SET quantity = v_stock 
-            WHERE barCode = :new.barcode AND
-                  username = :new.username AND
-                  orderdate = :new.orderdate;
-        DBMS_OUTPUT.PUT_LINE('Stock is less than the order quantity');
+        :new.quantity := v_stock;
         SELECT count('x') 
             INTO v_replacement_order_count
         FROM replacements 
             WHERE barCode = :new.barCode AND status = 'D';
+
         IF v_replacement_order_count = 0 THEN
-            SELECT taxID, min(cost) 
-                INTO v_supplier, v_supplier_cost 
-            FROM supply_lines 
-                WHERE barcode = :new.barcode 
-                GROUP BY taxID 
-                HAVING cost=min(cost);
+            WITH min_costs AS (
+                SELECT taxID, MIN(cost) AS min_cost
+                FROM supply_lines 
+                    WHERE barcode = :new.barcode 
+                    GROUP BY taxID 
+            )
+            SELECT taxID, min_cost INTO v_supplier, v_supplier_cost 
+            FROM min_costs;
+
             INSERT INTO replacements (orderdate, barCode, taxID, status, units, payment) 
-            VALUES (SYSDATE, :new.barcode, v_supplier, 'D', v_max_stock/2, v_max_stock/2 * v_supplier_cost);
+            VALUES (SYSDATE, :new.barcode, NULL, 'D', v_max_stock/2, v_max_stock/2 * v_supplier_cost);
 		END IF;
-        
+
+        /*
         SELECT count('x') 
             INTO v_supply_lines_count
             FROM supply_lines
@@ -62,6 +50,7 @@ BEGIN
         IF v_supply_lines_count = 0 THEN
             INSERT INTO supply_lines (barCode) VALUES (:new.barcode);
         END IF;
+        */
     END IF;
 END;
 /
